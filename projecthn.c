ec1a3baf12a0f16517519b2b8e73203d454d66bc
hnlee <skyj0216@gmail.com>
2022-09-19 10:18:06 +0900

    project

--- projecthn.c
+++ projecthn.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+//1. 인풋에 연결한 순서 기억 
+//2. 연결 해제시 마지막으로 연결한 인풋을 아웃풋으로 설정
+//3. 기본 아웃풋값은 1번 인풋
+
 int main()
 {
-
-    printf("test 1");
-
+    printf("test");
+    printf("1");
     return 0;
 }
